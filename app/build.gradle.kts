cc707cd7265faa830d472c6d045e5654486a91b1
zahib <zahibshoukat524@gmail.com>
2024-06-28 20:58:43 +0500

    production release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.safe.besafe"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
